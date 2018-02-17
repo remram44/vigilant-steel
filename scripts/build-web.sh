a52a70e8357c90b0725884b7101f470c151399f6
Remi Rampin <remirampin@gmail.com>
2018-02-17 18:27:38 -0500

    Fix Emscripten build

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd "$(dirname "$0")/.."
-docker run -t --rm -v "$PWD:/src" -v "$HOME/.cargo/registry:/root/.cargo/registry" -e EMMAKEN_CFLAGS='-s USE_SDL=2' remram/emscripten-rust-sdl
+docker run -t --rm -v "$PWD:/src" -v "$HOME/.cargo/registry:/root/.cargo/registry" -e EMMAKEN_CFLAGS='-s USE_SDL=2' remram/emscripten-rust-sdl sh -c 'cd client-piston && cargo build --release --target=asmjs-unknown-emscripten --no-default-features'
