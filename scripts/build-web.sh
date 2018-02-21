c76cc124181af7357605ae85c0ff7ba3ec32b231
Remi Rampin <remirampin@gmail.com>
2018-02-20 19:12:38 -0500

    Make build script move data file

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -2,3 +2,4 @@
 
 cd "$(dirname "$0")/.."
 docker run -t --rm -v "$PWD:/src" -v "$HOME/.cargo/registry:/root/.cargo/registry" -e EMMAKEN_CFLAGS='-s USE_SDL=2 --preload-file assets' remram/emscripten-rust-sdl sh -c 'cd client-piston && cargo build --release --target=asmjs-unknown-emscripten --no-default-features'
+cp target/asmjs-unknown-emscripten/release/deps/client-*.data .
