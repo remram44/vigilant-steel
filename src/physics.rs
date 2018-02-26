67b32ab21f56fe663bde4b78bc212520cbd8c2b2
Remi Rampin <remirampin@gmail.com>
2018-02-26 18:49:13 -0500

    Rework SAT function

--- src/physics.rs
+++ src/physics.rs
@@ -110,33 +110,31 @@ impl<'a> System<'a> for SysSimu {
 /// Collision detection and response.
 pub struct SysCollision;
 
-/// Checks for collisions between non-axis-oriented rectangles.
+/// Checks if two shapes collide when projected on a specific axis.
 ///
-/// Uses SAT to check if two rectangles collide.
-// TODO: replace with better method
-fn check_sat_collision(
-    s_pos: &Position,
-    s_col: &Collision,
-    o_pos: &Position,
-    o_col: &Collision,
+/// This is part of the SAT collision detection method.
+fn check_sat_collision_dir(
+    pos1: &Position,
+    size1: &[f64; 2],
+    pos2: &Position,
+    size2: &[f64; 2],
     dir: [f64; 2],
 ) -> bool {
     // This is called for each normal of each rectangle
     // It checks whether there is collision of the shape projected along it
-    let sides = &[(-1.0, -1.0), (-1.0, 1.0), (1.0, 1.0), (1.0, -1.0)];
-    // Project S rectangle
-    let (s, c) = s_pos.rot.sin_cos();
-    let s_proj = sides
+
+    let corners = &[(-1.0, -1.0), (-1.0, 1.0), (1.0, 1.0), (1.0, -1.0)];
+    // Project rectangle 1
+    let (s, c) = pos1.rot.sin_cos();
+    let proj1 = corners
         .iter()
-        .map(|&(xs, ys)| {
+        .map(|&(x, y)| {
             // Compute corner coordinates
             let corner = vec2_add(
-                s_pos.pos,
+                pos1.pos,
                 [
-                    s_col.bounding_box[0] * xs * c
-                        + s_col.bounding_box[1] * ys * (-s),
-                    s_col.bounding_box[0] * xs * s
-                        + s_col.bounding_box[1] * ys * c,
+                    size1[0] * x * c + size1[1] * y * (-s),
+                    size1[0] * x * s + size1[1] * y * c,
                 ],
             );
             // Dot product with dir vector gives the distance along that vector
@@ -144,19 +142,17 @@ fn check_sat_collision(
         })
         .minmax()
         .unwrap();
-    // Project O rectangle
-    let (s, c) = o_pos.rot.sin_cos();
-    let o_proj = sides
+    // Project rectangle 2
+    let (s, c) = pos2.rot.sin_cos();
+    let proj2 = corners
         .iter()
-        .map(|&(xs, ys)| {
+        .map(|&(x, y)| {
             // Compute corner coordinates
             let corner = vec2_add(
-                o_pos.pos,
+                pos2.pos,
                 [
-                    o_col.bounding_box[0] * xs * c
-                        + o_col.bounding_box[1] * ys * (-s),
-                    o_col.bounding_box[0] * xs * s
-                        + o_col.bounding_box[1] * ys * c,
+                    size2[0] * x * c + size2[1] * y * (-s),
+                    size2[0] * x * s + size2[1] * y * c,
                 ],
             );
             // Dot product with dir vector gives the distance along that vector
@@ -165,7 +161,33 @@ fn check_sat_collision(
         .minmax()
         .unwrap();
 
-    s_proj.0 < o_proj.1 && o_proj.0 < s_proj.1
+    proj1.0 < proj2.1 && proj2.0 < proj1.1
+}
+
+/// Checks if two shapes collide when projected on a specific axis.
+///
+/// Uses SAT to check if two rectangles collide.
+fn check_sat_collision(
+    pos1: &Position,
+    size1: &[f64; 2],
+    pos2: &Position,
+    size2: &[f64; 2],
+) -> bool {
+    let (s, c) = pos1.rot.sin_cos();
+    if !check_sat_collision_dir(pos1, size1, pos2, size2, [c, s])
+        || !check_sat_collision_dir(pos1, size1, pos2, size2, [-s, c])
+    {
+        return false;
+    }
+
+    let (s, c) = pos2.rot.sin_cos();
+    if !check_sat_collision_dir(pos1, size1, pos2, size2, [c, s])
+        || !check_sat_collision_dir(pos1, size1, pos2, size2, [-s, c])
+    {
+        return false;
+    }
+
+    true
 }
 
 impl<'a> System<'a> for SysCollision {
@@ -191,48 +213,29 @@ impl<'a> System<'a> for SysCollision {
                     continue;
                 }
                 // Detect collisions using SAT
-                let (s_s, s_c) = s_pos.rot.sin_cos();
-                if check_sat_collision(s_pos, s_col, o_pos, o_col, [s_c, s_s])
-                    && check_sat_collision(
-                        s_pos,
-                        s_col,
-                        o_pos,
-                        o_col,
-                        [-s_s, s_c],
-                    ) {
-                    let (o_s, o_c) = o_pos.rot.sin_cos();
-                    if check_sat_collision(
-                        s_pos,
-                        s_col,
-                        o_pos,
-                        o_col,
-                        [o_c, o_s],
-                    )
-                        && check_sat_collision(
-                            s_pos,
-                            s_col,
-                            o_pos,
-                            o_col,
-                            [-o_s, o_c],
-                        ) {
-                        // Collision!
-                        let insert = if let Some(col) = collided.get_mut(s_e) {
-                            col.entities.push(o_e);
-                            false
-                        } else {
-                            true
-                        };
-                        if insert {
-                            collided.insert(
-                                s_e,
-                                Collided {
-                                    entities: vec![o_e],
-                                },
-                            );
-                        }
-                        #[cfg(feature = "network")]
-                        lazy.insert(s_e, net::Dirty);
+                if check_sat_collision(
+                    &s_pos,
+                    &s_col.bounding_box,
+                    &o_pos,
+                    &o_col.bounding_box,
+                ) {
+                    // Collision!
+                    let insert = if let Some(col) = collided.get_mut(s_e) {
+                        col.entities.push(o_e);
+                        false
+                    } else {
+                        true
+                    };
+                    if insert {
+                        collided.insert(
+                            s_e,
+                            Collided {
+                                entities: vec![o_e],
+                            },
+                        );
                     }
+                    #[cfg(feature = "network")]
+                    lazy.insert(s_e, net::Dirty);
                 }
             }
         }
