fd52c0fd48fc37ac7d315d683d5a45f30d785efa
Remi Rampin <remirampin@gmail.com>
2018-03-21 23:48:33 -0400

    Add bench

---
+++ benches/simple.rs
@@ -0,0 +1,20 @@
+#![feature(test)]
+
+extern crate game;
+extern crate test;
+
+use game::Game;
+use test::Bencher;
+
+#[bench]
+fn flying_asteroids(b: &mut Bencher) {
+    b.bytes = 12;
+    b.iter(|| {
+        let mut game = Game::new_standalone();
+        const TIME_STEP: f64 = 0.040;
+        // Simulate 20 seconds of game time
+        for frame in 0..500 {
+            game.update(TIME_STEP);
+        }
+    });
+}
